650ac84a294f476bdd2b4a385fb6000af6e29328
cimendes <iines.mendes@gmail.com>
2017-03-10 14:15:59 +0000

    Fixed big with allele number. comparisons all done as uppercase now. exits if schema is not found. fetches more than one shema if available.

--- modules/checkMLST.py
+++ modules/checkMLST.py
@@ -10,47 +10,51 @@ import csv
 import utils
 import rematch_module
 import utils
-from  __builtin__ import any
 from glob import glob
 import shutil
+import re
 
-def downloadPubMLSTxml(species, xmlURL='http://pubmlst.org/data/dbases.xml'):
+def downloadPubMLSTxml(originalSpecies, xmlURL='http://pubmlst.org/data/dbases.xml'):
 
-	print '\n' + 'Searching MLST database for ' + species
+	print '\n' + 'Searching MLST database for ' + originalSpecies
+
+	species=originalSpecies.upper()
 
 	try:
 		content = urllib2.urlopen(xmlURL)
 		xml = content.read()
 		tree = ET.fromstring(xml)
-		#print tree #Element Root
 	except:
 		print "Ooops!There might be a problem with the PubMLST service, try later or check if the xml is well formated at " + xmlURL
 		raise
 
 	xmlData={}
+
+	success=0
 	for scheme in tree.findall('species'):
-		try:
-			if scheme.text.strip() == species:
-				xmlData[scheme.text.strip()]={}
-				for info in scheme: #mlst
-					for database in info: #database
-						for retrievedDate in database.findall('retrieved'):
-							retrieved = retrievedDate.text
-							xmlData[scheme.text.strip()][retrieved]=[]
-						for profile in database.findall('profiles'):
-								profileURl=profile.find('url').text
-								xmlData[scheme.text.strip()][retrieved].append(profileURl)
-						for lociScheme in database.findall('loci'):
-							loci={}
-							for locus in lociScheme:
-								locusID=locus.text
-								for locusInfo in locus:
-									locusUrl=locusInfo.text
-									loci[locusID.strip()]=locusUrl
-								xmlData[scheme.text.strip()][retrieved].append(loci)
-		except:
-			print "No schema found for %s" % (species) #TODO - not working
-			raise
+		if scheme.text.strip().upper() == species or species in scheme.text.strip().upper():
+			success+=1
+			xmlData[scheme.text.strip()]={}
+			for info in scheme: #mlst
+				for database in info: #database
+					for retrievedDate in database.findall('retrieved'):
+						retrieved = retrievedDate.text
+						xmlData[scheme.text.strip()][retrieved]=[]
+					for profile in database.findall('profiles'):
+							profileURl=profile.find('url').text
+							xmlData[scheme.text.strip()][retrieved].append(profileURl)
+					for lociScheme in database.findall('loci'):
+						loci={}
+						for locus in lociScheme:
+							locusID=locus.text
+							for locusInfo in locus:
+								locusUrl=locusInfo.text
+								loci[locusID.strip()]=locusUrl
+							xmlData[scheme.text.strip()][retrieved].append(loci)
+	if success==0:
+		sys.exit("\tError. No schema found for %s" % (originalSpecies))
+	elif success>1:
+		print "\tWarning. More than one schema foind for %s. Loading both..." % (originalSpecies)
 
 	#uncomment for verbose
 	'''
@@ -64,33 +68,34 @@ def downloadPubMLSTxml(species, xmlURL='http://pubmlst.org/data/dbases.xml'):
 				print "Loci name: " + loci
 				print "Loci URL: " + url
 	'''
-	return xmlData
-
-def getSequences(URLdictionary):
-
-	ToSkip=False
-	STdict={}
-	SequenceDict={}
-
 	if not os.path.isdir('./pubmlst'):
 		os.makedirs('./pubmlst')
 
-	for SchemaName, info in URLdictionary.items():
-		#print SchemaName
+	out=[]
+
+	for SchemaName, info in xmlData.items():
+
+		STdict={}
+		SequenceDict={}
+
 		for RetrievalDate, URL in info.items():
-			#print RetrievalDate
 			outDit='./pubmlst'+'/'+SchemaName.replace(' ','_')+'_'+RetrievalDate #compatible with windows? See if it already exists, if so, break
+			
 			if os.path.isdir(outDit):
 				print "\tschema files already exist for %s" % (SchemaName)
-				ToSkip=True
+				toSave=utils.extractVariableFromPickle(outDit+'/'+SchemaName.replace(' ','_')+'_'+RetrievalDate+'.pkl')
+				out.append(toSave)
 				break
+			
 			elif any(SchemaName.replace(' ','_') in x for x in os.listdir('./pubmlst/')):
 				print "Older version of %s's scheme found! Deleting..." % (SchemaName)
 				for directory in glob(str('./pubmlst'+'/'+SchemaName.replace(' ','_')+'_*')):
 					shutil.rmtree(directory)
 					os.makedirs(outDit)
+			
 			else:
 				os.makedirs(outDit)
+			
 			contentProfile=urllib2.urlopen(URL[0])
 			profileFile =  csv.reader(contentProfile, delimiter='\t')
 			profileFile.next() #skip header
@@ -104,10 +109,13 @@ def getSequences(URLdictionary):
 				contentLoci=urllib.urlretrieve(lociURL, outDit+'/'+os.path.basename(lociURL))
 				sequences, headers=rematch_module.get_sequence_information(outDit+'/'+os.path.basename(lociURL),0)
 				for key in sequences.keys():
-					header=sequences[key]['header'].split('_')[-1]
-					sequence=sequences[key]['sequence']
+					header=re.sub("\D", "", sequences[key]['header'])
+					sequence=sequences[key]['sequence'].upper()
 					SequenceDict[lociName][sequence]=header
 				os.remove(outDit+'/'+os.path.basename(lociURL))
+			toSave=[SequenceDict,STdict]
+			utils.saveVariableToPickle(toSave,outDit,SchemaName.replace(' ','_')+'_'+RetrievalDate)
+			out.append(toSave)
 	'''
 	for k,v in STdict.items():
 		print v + '->' + k
@@ -117,38 +125,40 @@ def getSequences(URLdictionary):
 		for key,value in v.items():
 			print value
 			print key
-	'''
+	
 	if not ToSkip:
 		toSave=[SequenceDict,STdict]
 		utils.saveVariableToPickle(toSave,outDit,SchemaName.replace(' ','_')+'_'+RetrievalDate)
+		return True
+	else:
+		return False
 
-
-
-
-
-
+	'''
+	#print SequenceDict, STdict
+	#print len(out)
+	return out
 
 def main():
-	agalactiae=downloadPubMLSTxml('Streptococcus agalactiae')
-	getSequences(agalactiae)
+	'''agalactiae=downloadPubMLSTxml('Streptococcus agalactiae')
 
 	pyogenes=downloadPubMLSTxml('Streptococcus pyogenes')
-	getSequences(pyogenes)
 
 	pneumo=downloadPubMLSTxml('Streptococcus pneumoniae')
-	getSequences(pneumo)
-
+	
 	aspergillus=downloadPubMLSTxml('Aspergillus fumigatus')
-	getSequences(aspergillus)
-
+	'''
 	salmonella=downloadPubMLSTxml('Salmonella enterica')
-	getSequences(salmonella)
+	
+	yersinia=downloadPubMLSTxml('Yersinia pseudotuberculosis')
 
 	campy=downloadPubMLSTxml('Campylobacter jejuni')
-	getSequences(campy)
-	
-	downloadPubMLSTxml('lala') #TODO
 
+	acineto, lala=downloadPubMLSTxml('Acinetobacter baumannii')
+	#print acineto
+	#print lala
+
+	downloadPubMLSTxml('lala') #TODO
+	
 if __name__ == "__main__":
     main()
 
