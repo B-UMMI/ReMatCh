3eb8305bc52b43f782a75b98bc58144eb067e8be
miguelpmachado <mpmachado@fc.ul.pt>
2017-04-20 14:41:31 +0100

    Fix bug

--- modules/rematch_module.py
+++ modules/rematch_module.py
@@ -150,7 +150,7 @@ def verify_mapped_tip(reference_length, mapping_position, read_length, cigar):
 
 def change_sam_flag_bit_mapped_reverse_strand_2_direct_strand(sam_flag_bit):
 	bit = list(format(sam_flag_bit, 'b').zfill(5))
-	bit[-5] = '1'
+	bit[-5] = '0'
 	return int(''.join(bit), 2)
 
 
