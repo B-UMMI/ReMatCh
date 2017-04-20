c0c4863e3bf5a098ae642e828a3193e6eef63d17
miguelpmachado <mpmachado@fc.ul.pt>
2017-04-20 16:43:12 +0100

    Do not look to tips

--- modules/rematch_module.py
+++ modules/rematch_module.py
@@ -177,7 +177,8 @@ def parallelized_recode_soft_clipping(line_collection, pickleFile, softClip_base
 				lines_sam.append(line)
 			else:
 				line = line.split('\t')
-				if not verify_mapped_tip(sequences_length[line[2]], int(line[3]), len(line[9]), line[5]):
+				# if not verify_mapped_tip(sequences_length[line[2]], int(line[3]), len(line[9]), line[5]):
+				if 'S' in line[5]:
 					line[5], line[3] = recode_cigar_based_on_base_quality(line[5], line[10], softClip_baseQuality, int(line[3]), verify_mapped_direct_strand(int(line[1])), softClip_cigarFlagRecode)
 				lines_sam.append('\t'.join(line))
 	with open(pickleFile, 'wb') as writer:
