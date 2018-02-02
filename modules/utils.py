acee7cba3b3ea5ba19927a5bfddd19b4f5dfa588
miguelpmachado <mpmachado@fc.ul.pt>
2018-02-02 14:10:22 +0000

    Make subprocess exceptions more descriptive

--- modules/utils.py
+++ modules/utils.py
@@ -233,9 +233,25 @@ def trace_unhandled_exceptions(func):
     def wrapped_func(*args, **kwargs):
         try:
             func(*args, **kwargs)
-        except:
-            print 'Exception in ' + func.__name__
+        except Exception as e:
+            print('Exception in ' + func.__name__)
+            print(e)
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            traceback_details = {
+                                 'filename': exc_traceback.tb_frame.f_code.co_filename,
+                                 'lineno': exc_traceback.tb_lineno,
+                                 'name': exc_traceback.tb_frame.f_code.co_name,
+                                 'type': exc_type.__name__,
+                                 'message': exc_value.message,  # or see traceback._some_str()
+                                }
+            del (exc_type, exc_value, exc_traceback)
+            traceback_template = '''
+            Traceback (most recent call last):
+            File "%(filename)s", line %(lineno)s, in %(name)s %(type)s: %(message)s\n
+            '''
             traceback.print_exc()
+            print(traceback.format_exc())
+            print(traceback_template % traceback_details)
     return wrapped_func
 
 
