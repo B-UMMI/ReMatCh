1f2893bddc1c46a7d8a88cf61db5677d13a31f18
miguelpmachado <mpmachado@fc.ul.pt>
2018-02-03 08:19:11 +0000

    Clean utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -228,35 +228,6 @@ def extractVariableFromPickle(pickleFile):
     return variable
 
 
-'''
-def trace_unhandled_exceptions(func):
-    @functools.wraps(func)
-    def wrapped_func(*args, **kwargs):
-        try:
-            func(*args, **kwargs)
-        except Exception as e:
-            print('Exception in ' + func.__name__)
-            print(e)
-            exc_type, exc_value, exc_traceback = sys.exc_info()
-            traceback_details = {
-                                 'filename': exc_traceback.tb_frame.f_code.co_filename,
-                                 'lineno': exc_traceback.tb_lineno,
-                                 'name': exc_traceback.tb_frame.f_code.co_name,
-                                 'type': exc_type.__name__,
-                                 'message': exc_value.message,  # or see traceback._some_str()
-                                }
-            del (exc_type, exc_value, exc_traceback)
-            traceback_template = 
-            Traceback (most recent call last):
-            File "%(filename)s", line %(lineno)s, in %(name)s %(type)s: %(message)s\n
-            
-            traceback.print_exc()
-            print(traceback.format_exc())
-            print(traceback_template % traceback_details)
-    return wrapped_func
-'''
-
-
 def trace_unhandled_exceptions(func):
     @functools.wraps(func)
     def wrapped_func(*args, **kwargs):
