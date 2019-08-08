ffb21bb012be75822842ce086abab6e0e01bfd25
miguelpmachado <mpmachado@fc.ul.pt>
2019-08-08 11:17:17 +0100

    Correct ReMatCh utils import

--- ReMatCh/modules/rematch_module.py
+++ ReMatCh/modules/rematch_module.py
@@ -3,10 +3,10 @@ import multiprocessing
 import functools
 import sys
 import pickle
-from . import utils
 
 # https://chrisyeh96.github.io/2017/08/08/definitive-guide-python-imports.html#case-2-syspath-could-change
 sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
+import utils
 
 
 def index_fasta_samtools(fasta, region_none, region_outfile_none, print_comand_true):
